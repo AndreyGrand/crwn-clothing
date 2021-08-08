8e8b3fc204dc05c77e2b2bc5a496dda2ad29f5fb
andrey gerankin <gerankin@mail.ru>
2021-08-08 20:01:57 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
-const HatsPage = () => (
+const HatsPage = props => {
+  console.log(props);
+  return(
   <div>
     <h1>HATS PAGE</h1>
   </div>
 );
-
+};
+const TimePage = props => {
+  console.log(props);
+  return(
+  <div>
+    <h1>Time PAGE {props.match.params.hatId}</h1>
+  </div>
+);
+};
 function App() {
   return (
     <div>
     <Switch>
         <Route exact={true} path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+        <Route path='/hats/:hatId' component={TimePage}/>
     </Switch>
     </div>
   );
